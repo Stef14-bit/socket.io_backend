8c3475100eeb7bfaafa15ceb0397deaa10b31320
stefan <stefan@stefan.com>
2023-01-20 01:26:41 +0100

    connection established

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const socketIO = require("socket.io");
 
 const server = app.listen(3001);
 
-const io = socketIO(server, { cors: { origin: ["http://localhost:3000"] } });
-const messages = [{ id: uniqid(), aiuthor: server, text: "welcome to chat" }];
+const io = socketIO(server, {
+  cors: { origin: ["http://localhost:3000"] },
+});
+
+const messages = [
+  { id: uniqid(), author: "server", text: "welcome to WildChat" },
+];
 
 io.on("connect", (socket) => {
   console.log("user connected");
-  socket.on("disconect", () => {
+  socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
